73077f7cc3dd2560028acf419fd4948ba6e82009
Christian Stemmann <stemmi@sanktemmi.de>
2023-04-30 09:55:28 +0200

    refactor: use new api path /api/ +plural

--- src/services/dataGetters.js
+++ src/services/dataGetters.js
@@ -1,61 +1,61 @@
 import settings from "./settings";
 
 async function getBands() {
-    const response = await fetch(settings.baseUrl()+"/band");
+    const response = await fetch(settings.apiUrl()+"/bands");
     const bands = await response.json();
     return bands.results;    
 }
 
 async function getBandsByShowId(showId) {
-    const response = await fetch(settings.baseUrl()+"/show/"+showId+"/bands");
+    const response = await fetch(settings.apiUrl()+"/shows/"+showId+"/bands");
     const bands = await response.json();
     return bands.results;    
 }
 
 async function getLocations() {
-    const response = await fetch(settings.baseUrl()+"/location");
+    const response = await fetch(settings.apiUrl()+"/locations");
     const locations = await response.json();
     return locations.results;
 }
 
 async function getShows() {
-    const response = await fetch(settings.baseUrl()+"/show");
+    const response = await fetch(settings.apiUrl()+"/shows");
     const shows = await response.json();
     return shows.results;
 }
 
 async function getUsers() {
-    const response = await fetch(settings.baseUrl()+"/user");
+    const response = await fetch(settings.apiUrl()+"/users");
     const users = await response.json();
     return users.results;
 }
 
 async function getLocationById(id) {
-    const response = await fetch(settings.baseUrl()+"/location/" + id);
+    const response = await fetch(settings.apiUrl()+"/locations/" + id);
     const location = await response.json();
     return location;
 }
 
 async function getShowById(id) {
-    const response = await fetch(settings.baseUrl()+"/show/" + id);
+    const response = await fetch(settings.apiUrl()+"/shows/" + id);
     const show = await response.json();
     return show;
 }
 
 async function getUserById(id) {
-    const response = await fetch(settings.baseUrl()+"/user/" + id);
+    const response = await fetch(settings.apiUrl()+"/users/" + id);
     const user = await response.json();
     return user;
 }
 
 async function getLatestLocationPosition() {
-    const response = await fetch(settings.baseUrl()+"/location/latest");
+    const response = await fetch(settings.apiUrl()+"/locations/latest");
     const latestLocation = await response.json();
     return [latestLocation.long, latestLocation.lat];
 }
 
 async function getShowsByLocationId(locationId) { 
-    const response = await fetch(settings.baseUrl()+"/location/" + locationId + "/shows");
+    const response = await fetch(settings.apiUrl()+"/locations/" + locationId + "/shows");
     const shows = await response.json();
     return shows.results;
 }
--- src/services/settings.js
+++ src/services/settings.js
@@ -2,7 +2,13 @@ function baseUrl() {
     return "http://localhost:3000";
 }
 
+function apiUrl() {
+    return baseUrl()+"/api";
+}
+
+console.log(apiUrl());
 
 export default {
-    baseUrl
+    baseUrl,
+    apiUrl
 }
