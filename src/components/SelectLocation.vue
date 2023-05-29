e360d141f014656fd8439d303c94db222479a3ab
Christian Stemmann <stemmi@sanktemmi.de>
2023-05-29 14:32:13 +0200

    refactor: location form handling

---
+++ src/components/SelectLocation.vue
@@ -0,0 +1,49 @@
+<template>
+    <label for="loc_id">Location:</label><br>
+    
+    <select v-if="locations" id="loc_id" name="loc_id" required :value="location_id" @input="selectLocation" >
+        <option v-for="location of locations" :key="location.id" :value="location.id">{{ [ location.city, location.name ].filter(item => item).join(', ') }}</option>
+        <option value="-1">+++ Neue Location +++</option>
+    </select><br><br>
+
+    <NewLocationForm v-if="location_id===-1" @new_location_id="setNewLocationId"/>       
+</template>
+
+<script>
+    import NewLocationForm from './NewLocationForm.vue';
+    import api from '../services/api.js';
+
+
+    export default {
+        data() {
+            return {
+                locations: undefined
+            }
+        },
+        components: {
+            NewLocationForm
+        },
+        emits: [
+            "updateLocation"
+        ],
+        props: [
+            "location_id"
+        ],
+        methods: {
+            selectLocation(event) {
+                console.log('value', event.target.value); // jshdgfjwgfhsjhg
+                this.$emit("updateLocation", event.target.value);
+            },
+            async setNewLocationId(id) {
+                this.locations = await api.getLocations();
+                this.$emit("updateLocation", id);
+            }
+        },
+        async mounted() {            
+            this.locations = await api.getLocations();
+        }
+    } 
+</script>
+
+<style scoped>
+</style>
--- src/components/ShowForm.vue
+++ src/components/ShowForm.vue
@@ -3,13 +3,7 @@
         <label for="title">Titel (optional):</label><br>
         <input type="text" id="title" name="title" maxlength="255" size="40"><br><br>
         
-        <label for="location_id">Location:</label><br>
-        <select v-model="location_id" v-if="locations" id="location_id" name="location_id" required>
-            <option v-for="location of locations" :key="location.id" :value="location.id">{{ [ location.city, location.name ].filter(item => item).join(', ') }}</option>
-            <option value="-1">+++ Neue Location +++</option>
-        </select><br><br>
-
-        <LocationForm v-if="location_id==='-1'" @new_location_id="updateLocationId"/>
+        <SelectLocation :location_id="location_id" @update-location="handleLocationSelection" />
 
         <label for="date">Datum:</label><br>
         <input type="date" id="date" name="date" size="10" required><br><br>
@@ -23,69 +17,39 @@
         <label for="poster_alt">Plakat-Alt-Text:</label><br>
         <input type="text" id="poster_alt" name="poster_alt" maxlength="255" size="40"><br><br>
 
-        <label for="band_id">Weitere Bands:</label><br>
-        <select v-model="band_id" v-if="locations" id="band_id" name="band_id">
-            <option v-for="band of allBands" :key="band.id" :value="band.id">{{ band.name }}</option>
-            <option value="-1">+++ Neue Band +++</option>
-        </select>
-        <button class="inline_button" type="button" @click="addBand">+</button><br><br>
-
-        <ul class="bands-text">
-            <li v-for="band, index in bandsList" :key="band.id">{{ band.name }}<button class="inline_button" type="button" :data-index="index" @click="removeBand">x</button></li>
-        </ul><br><br>
+        <SelectBands />
 
         <input type="hidden" id="user_id" name="user_id" value="1">
         
         <button v-if="location_id != -1" type="submit">OK</button>
-
     </form>
 </template>
 
 <script>
-    import LocationForm from './LocationForm.vue';
-    import formatting from '../services/formatting.js';
+    import SelectLocation from './SelectLocation.vue';
+    import SelectBands from './SelectBands.vue';
     import settings from '../services/settings.js';
-    import api from '../services/api.js';
-
 
     export default {
         data() {
             return {
                 apiUrl: settings.apiUrl(),
-
-                locations: undefined,
-                location_id: undefined,
-                allBands: undefined,
-                selectedBandIds: [],
-                band_id: undefined,
+                location_id: undefined
             }
         },
         components: {
-            LocationForm
-        },
-        computed: {
-            bandsList() {
-                return formatting.createBandsList(this.allBands, this.selectedBandIds);
-            }
+            SelectLocation, SelectBands
         },
         methods: {
-            async updateLocationId(id) {
-                this.locations = await api.getLocations();
-                this.location_id = id;
-            },
-            addBand() {
-                if (this.selectedBandIds.includes(this.band_id)) return;
-                this.selectedBandIds.push(this.band_id);
-            },
-            removeBand(event) {
-                this.selectedBandIds.splice(event.target.dataset.index, 1);
+            handleLocationSelection(value) {
+                this.location_id = +value;
             },
             async postShow(event) {
                 event.preventDefault();
 
                 const reqBody = {
                     'title': title.value,
-                    'location_id': location_id.value,
+                    'location_id': this.location_id.toString(),
                     'date': date.value,
                     'text': text.value,
                     'poster_filename': poster_filename.value,
@@ -103,31 +67,10 @@
                 .then((response) => response.json())
                 .then((show) => this.$router.push({ path: `/detail/${show.id}` }));
             }
-        },
-        async mounted() {            
-            this.locations = await api.getLocations();
-            this.allBands = await api.getBands();
-
         }
     } 
 </script>
 
 <style scoped>
-    ul {
-        list-style-type: none;
-        display: flex;
-        gap: 10px;
-    } 
-    .inline_button {
-        font-weight: bold;
-        display:inline-block;
-        margin: auto 5px auto 5px;
-        padding: 1px 5px 2px 5px;
-    }
-
-    .bands-text {
-        background-color: white;
-        font-size: 0.9rem;
-        color: black;
-    }
+    
 </style>
