c5a0981e51a3b27836a5582f8cf3ebd90bf3b3ce
Christian Stemmann <stemmi@sanktemmi.de>
2023-06-05 21:08:07 +0200

    feat: add edit Component (wip)

---
+++ src/views/Edit.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+    <section>
+    
+    <h3>Eintrag bearbeiten:</h3>
+    <ShowForm v-if="show&&bands" :show="show" :bands="bands" />
+    
+    </section>
+    </main>
+</template>
+
+<script>
+    import ShowForm from '../components/ShowForm.vue';
+    import api from '../services/api';
+
+    export default {
+        components: { 
+            ShowForm
+        },
+        data() {
+            return {
+                show: undefined,
+                bands: undefined
+            }
+        },
+        async mounted() {
+            this.show = await api.getShowById(this.$route.params.id);
+            this.bands = await api.getBandsByShowId(this.show.id);
+        }
+    } 
+</script>
+
+<style scoped>
+    h3 {
+        margin-bottom: 20px;
+    }
+</style>
